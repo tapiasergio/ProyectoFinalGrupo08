c41aa3bc9120262ce40dab74915958d5ebb16026
tapiasergio <sergiotapia782@gmail.com>
2022-11-13 21:20:44 -0300

    Agregando Desarrolladores
    Implementando Desarrolladores a la pagina

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
+=======
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+>>>>>>> Stashed changes
 import './App.css';
 
 function App() {
