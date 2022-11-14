b486e0805184c0e783966518cb785ae85c2a3ce0
xAtilio00 <110836233+xAtilio00@users.noreply.github.com>
2022-11-14 03:26:48 -0300

    Subida de carpeta y archivos ppt

---
+++ src/Component/PPTGame/Img/ImgData.js
@@ -0,0 +1,18 @@
+import Tijera from './Tijera.png';
+import Papel from './Papel.png';
+import Piedra from './Piedra.png';
+
+export default [
+    {
+        'id': 1,
+        'src':Tijera
+    },
+    {
+        'id': 2,
+        'src':Papel
+    },
+    {
+        'id': 3,
+        'src':Piedra
+    }
+]
